73cfe59d1cba64d9728f2018f4d58ee9cf68fe27
vendatt0r <vasyok03020454@gmail.com>
2025-04-01 23:24:17 +0300

    new

--- recruto/urls.py
+++ recruto/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from myapp.views import hello
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', hello),
 ]
